3a09ff04ea674c4743b10326dec319c91ae2ce9c
Antonio Odore <antohview@gmail.com>
2025-02-20 17:44:18 +0100

    MovementMolly And Other Assets

---
+++ Assets/PlayerMoviment.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+
+        // Controllo se il personaggio sta camminando
+        if (moveInput != Vector2.zero)
+        {
+            animator.SetBool("isWalking", true);
+            animator.SetFloat("InputX", moveInput.x);
+            animator.SetFloat("InputY", moveInput.y);
+
+            // Salva la direzione dell'ultimo movimento per l'animazione di idle
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
