0c199a0b6e1faae1a00089e844032afc1e58845e
Orso-bit <giovannijr2000@gmail.com>
2025-02-25 16:44:58 +0100

    TextBox
    TypeEffect and Space button to move to the second dialogue

--- Assets/TutorialInfo/Scripts/DialogueUI.cs
+++ Assets/TutorialInfo/Scripts/DialogueUI.cs
@@ -1,16 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
+
 
 public class DialogueUi : MonoBehaviour
 {
 
     [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+    private TypeEffect typeEffect;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        textLabel.text = "first line\nsecond line";
+        typeEffect = GetComponent<TypeEffect>();
+        ShowDialogue(testDialogue);
     }
-
-
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject) 
+    {
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return typeEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+    }
 }
