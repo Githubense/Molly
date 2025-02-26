89f745ea6cb3a1981b0108206456e3dc35d4fa5f
Gianpietro <panico.gianpietro@gmail.com>
2025-02-26 19:19:50 +0100

    Interaction

---
+++ Assets/Bed.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Bed : MonoBehaviour, IInteractable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Interact(){
+
+
+    }
+
+    public bool CanInteract(){
+
+            return true;
+    }
+}
