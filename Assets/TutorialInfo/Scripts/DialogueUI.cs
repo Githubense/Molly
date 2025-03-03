baaa7f827afe6d267bf08f72b91133777b7ce8c3
Orso-bit <giovannijr2000@gmail.com>
2025-03-03 20:25:55 +0100

    textbox
    Yes/No fixed

--- Assets/TutorialInfo/Scripts/DialogueUI.cs
+++ Assets/TutorialInfo/Scripts/DialogueUI.cs
@@ -25,30 +25,44 @@ public class DialogueUi : MonoBehaviour
     }
     public void ShowDialogue(DialogueObject dialogueObject)
     {
+        isOpen = true;
         dialogueBox.SetActive(true);
         StartCoroutine(StepThroughDialogue(dialogueObject));
     }
     private IEnumerator StepThroughDialogue(DialogueObject dialogueObject) 
+{
+    for (int i = 0; i < dialogueObject.Dialogue.Length; i++)
     {
-        for (int i = 0; i < dialogueObject.Dialogue.Length; i++)
+        string dialogue = dialogueObject.Dialogue[i];
+        yield return typeEffect.Run(dialogue, textLabel);
+
+        // Check if we are at the last dialogue and if it has responses.
+        if (i == dialogueObject.Dialogue.Length - 1 && dialogueObject.Responses != null && dialogueObject.HasResponses) 
         {
-            string dialogue = dialogueObject.Dialogue[i];
-            yield return typeEffect.Run(dialogue, textLabel);
-
-            if(i == dialogueObject.Dialogue.Length - 1 && dialogueObject.Responses != null && dialogueObject.HasResponses) break;
-
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            break; // Exit loop to show responses after the last dialogue step
         }
 
-        if(dialogueObject.HasResponses) 
-        {
-            responseHandler.ShowResponses(dialogueObject.Responses);
-        }
-        else 
-        {
-            CloseDialogueBox();
-        }
+        // Wait for the player to press space before continuing
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
     }
+
+    if (dialogueObject.HasResponses) 
+    {
+        // Show responses
+        responseHandler.ShowResponses(dialogueObject.Responses);
+
+        // Wait for the player to press space after responses to close the dialogue box
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
+        // After pressing space, close the dialogue box
+        CloseDialogueBox();
+    }
+    else 
+    {
+        // No responses, just close the dialogue box
+        CloseDialogueBox();
+    }
+}
         private void CloseDialogueBox()
         {
             isOpen = false;
