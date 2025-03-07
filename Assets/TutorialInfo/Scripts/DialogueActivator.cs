d18e2baf8dc14ab56e5f27ecf3d73efb44a65e3c
Orso-bit <giovannijr2000@gmail.com>
2025-03-07 17:47:31 +0100

    icon on the interactable object

--- Assets/TutorialInfo/Scripts/DialogueActivator.cs
+++ Assets/TutorialInfo/Scripts/DialogueActivator.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class DialogueActivator : MonoBehaviour, IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject;
+    [SerializeField] private GameObject visualCue;  // Reference to the visual cue object
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player") && other.TryGetComponent(out PlayerMovement player))
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerMovement player))
         {
             player.Interactable = this;
+
+            // Enable the visual cue when player is near
+            if (visualCue != null)
+                visualCue.SetActive(true);
         }
     }
 
@@ -19,6 +24,10 @@ public class DialogueActivator : MonoBehaviour, IInteractable
             if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this) // Fixed comparison
             {
                 player.Interactable = null;
+                
+                // Disable the visual cue when player leaves
+                if (visualCue != null)
+                    visualCue.SetActive(false);
             }
         }
     }
