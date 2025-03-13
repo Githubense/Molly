7b98341e5e7cb98aae7f3ffbbb358b2776eb0424
Antonio Odore <antohview@gmail.com>
2025-03-13 21:18:10 +0100

    always the god

--- Assets/Scripts/DialogueSystem/DialogueUI.cs
+++ Assets/Scripts/DialogueSystem/DialogueUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
+using UnityEngine.InputSystem; // Import new Input System namespace
 
 public class DialogueUi : MonoBehaviour
 {
@@ -14,6 +15,26 @@ public class DialogueUi : MonoBehaviour
     
     // UnityEvent to notify when dialogue is closed (optional)
     public UnityEvent OnDialogueClosed;
+
+    private InputAction skipDialogueAction; // Declare an InputAction for the Space key
+
+    private void Awake()
+    {
+        // Set up the input action for the Space key
+        skipDialogueAction = new InputAction(binding: "<Keyboard>/space");
+    }
+
+    private void OnEnable()
+    {
+        // Enable the input action
+        skipDialogueAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input action when not needed
+        skipDialogueAction.Disable();
+    }
 
     private void Start()
     {
@@ -39,11 +60,11 @@ public class DialogueUi : MonoBehaviour
             yield return typeEffect.Run(dialogueLine, textLabel);
 
             // Wait for user to press Space before showing the next line
-            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            yield return new WaitUntil(() => skipDialogueAction.triggered);
         }
 
         // After the last line, pressing Space again will close
-        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        yield return new WaitUntil(() => skipDialogueAction.triggered);
         CloseDialogueBox();
     }
 
