d72c2d28e5b7aa084c96c23e3ba279d1273653cc
Githubense <angelopf@outlook.com>
2025-03-19 17:56:41 +0100

    i moved a lot
    ewjibfshildkjs

--- Assets/Scripts/DialogueSystem/DialogueUI.cs
+++ Assets/Scripts/DialogueSystem/DialogueUI.cs
@@ -10,6 +10,8 @@ public class DialogueUi : MonoBehaviour
 {
     [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private GameObject choiceBox;
+    [SerializeField] private TMP_Text[] choiceLabels;
 
     public bool isOpen { get; private set; }
 
@@ -17,20 +19,31 @@ public class DialogueUi : MonoBehaviour
     public UnityEvent OnDialogueClosed;
 
     private InputAction skipDialogueAction;
+    private InputAction navigateAction;
+    private InputAction selectAction;
+
+    private int currentChoiceIndex;
+    private bool choiceSelected;
 
     private void Awake()
     {
         skipDialogueAction = new InputAction(binding: "<Keyboard>/space");
+        navigateAction = new InputAction(binding: "<Gamepad>/dpad");
+        selectAction = new InputAction(binding: "<Gamepad>/buttonSouth");
     }
 
     private void OnEnable()
     {
         skipDialogueAction.Enable();
+        navigateAction.Enable();
+        selectAction.Enable();
     }
 
     private void OnDisable()
     {
         skipDialogueAction.Disable();
+        navigateAction.Disable();
+        selectAction.Disable();
     }
 
     private void Start()
@@ -58,15 +71,95 @@ public class DialogueUi : MonoBehaviour
             yield return new WaitUntil(() => skipDialogueAction.triggered);
         }
 
-        yield return new WaitUntil(() => skipDialogueAction.triggered);
+        if (dialogueObject.HasChoices)
+        {
+            ShowChoices(dialogueObject.ChoiceKeys);
+            yield return new WaitUntil(() => choiceSelected);
+            HandleChoiceSelection(dialogueObject);
+        }
+
         CloseDialogueBox();
+    }
+
+    private void ShowChoices(string[] choiceKeys)
+    {
+        choiceBox.SetActive(true);
+        for (int i = 0; i < choiceKeys.Length; i++)
+        {
+            if (i < choiceLabels.Length)
+            {
+                var localizedString = new LocalizedString("DialogueTable", choiceKeys[i]);
+                choiceLabels[i].text = localizedString.GetLocalizedString();
+                choiceLabels[i].gameObject.SetActive(true);
+            }
+        }
+        // Hide any unused choice labels
+        for (int i = choiceKeys.Length; i < choiceLabels.Length; i++)
+        {
+            choiceLabels[i].gameObject.SetActive(false);
+        }
+        currentChoiceIndex = 0;
+        UpdateChoiceSelection();
+    }
+
+    private void UpdateChoiceSelection()
+    {
+        for (int i = 0; i < choiceLabels.Length; i++)
+        {
+            choiceLabels[i].color = i == currentChoiceIndex ? Color.yellow : Color.white;
+        }
     }
 
     private void CloseDialogueBox()
     {
         isOpen = false;
         dialogueBox.SetActive(false);
+        choiceBox.SetActive(false);
         textLabel.text = string.Empty;
         OnDialogueClosed?.Invoke();
+        choiceSelected = false; // Reset the choiceSelected flag
+    }
+
+    private void Update()
+    {
+        if (!isOpen) return;
+
+        if (navigateAction.triggered)
+        {
+            Vector2 navigation = navigateAction.ReadValue<Vector2>();
+            if (navigation.y > 0)
+            {
+                currentChoiceIndex = Mathf.Max(currentChoiceIndex - 1, 0);
+            }
+            else if (navigation.y < 0)
+            {
+                currentChoiceIndex = Mathf.Min(currentChoiceIndex + 1, choiceLabels.Length - 1);
+            }
+            UpdateChoiceSelection();
+        }
+
+        if (selectAction.triggered)
+        {
+            // Trigger the selection action
+            choiceSelected = true;
+        }
+    }
+
+    private void HandleChoiceSelection(DialogueObject dialogueObject)
+    {
+        string choiceResultKey = dialogueObject.GetChoiceResultKey(currentChoiceIndex);
+        if (!string.IsNullOrEmpty(choiceResultKey))
+        {
+            var localizedString = new LocalizedString("DialogueTable", choiceResultKey);
+            string resultLine = localizedString.GetLocalizedString();
+            StartCoroutine(DisplayResult(resultLine));
+        }
+    }
+
+    private IEnumerator DisplayResult(string resultLine)
+    {
+        yield return typeEffect.Run(resultLine, textLabel);
+        yield return new WaitUntil(() => skipDialogueAction.triggered);
+        CloseDialogueBox();
     }
 }
